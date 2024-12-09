8db3421239d701d73a416c479ef8313924ccc7bf
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2024-12-09 18:58:22 +0200

    fixed newsfeed and date in newscard

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import TopHeadlines from './components/chatgpt';
 import Newsfeed from "./pages/Newsfeed";
 import Events from "./pages/Events";
 import Opinion from "./pages/Opinion";
@@ -10,7 +9,15 @@ import About from "./pages/aboutUs";
 
 function App() {
   return (
-    <Newsfeed/>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Newsfeed />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/opinion" element={<Opinion />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -11,7 +11,7 @@ const NewsCard = ({ imageUrl,date, title, snippet }) => {
 
         <div className="relative bg-gradient-to-t from-gray-900/50 to-gray-900/25 pt-32 sm:pt-48 lg:pt-64">
           <div className="p-4 sm:p-6">
-            <time datetime="2022-10-10" className="block text-xs text-white/90"> {date} </time>
+            <time dateTime= {date} className="block text-xs text-white/90"> {date} </time>
 
             <a href="#">
               <h3 className="mt-0.5 text-lg text-white">{title}</h3>
