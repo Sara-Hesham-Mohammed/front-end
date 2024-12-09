2381c1382f25dbc4d59656e39fbdce5a5bcffdf4
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2024-12-10 01:24:15 +0200

    Categories, lsa el conditional rendering tho

--- src/pages/Newsfeed.jsx
+++ src/pages/Newsfeed.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import NewsCard from "../components/NewsCard";
 import Loader from "../UI/Loader";
-import axios from 'axios'; // Ensure axios is installed via npm
+import axios from 'axios'; 
 import Tabs from "../components/Tabs";
+import ErrorPage from "./Error";
+import CategoriesBar from "../components/CategoriesBar";
 
 
 const Newsfeed = () => {
 
-  /*** this section fe moshkela, it doesnt render ay haga, fix tthe API then we'll see***/ 
   const [news, setNews] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -25,6 +26,7 @@ const Newsfeed = () => {
         setLoading(false);  // Stop loading once data is fetched
       } catch (err) {
         setLoading(false);
+        return <ErrorPage/>
       }
     };
 
@@ -32,24 +34,43 @@ const Newsfeed = () => {
   }, []);  // Empty array means this runs once when the component mounts
 
   if (loading) return <Loader/>;
-  /******/ 
+
+  const tabOptions = [
+    { label: 'Settings', href: '#settings', isSelected: false },
+    { label: 'Messages', href: '#messages', isSelected: false },
+    { label: 'Archive', href: '#archive', isSelected: false },
+    { label: 'Notifications', href: '#notifications', isSelected: true },
+  ];
 
   return (
 
     //this but map it to ur component
     <>
-    <Tabs>
-    </Tabs>
-      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-        {news.map((article, index) => 
-        <NewsCard
-          key={index}
-          imageUrl={article.photo_url}
-          date={article.published_datetime_utc}
-          title={article.title}
-          snippet={article.snippet}
-        />)}
-      </div>
+   <CategoriesBar/>
+      <Tabs>
+      </Tabs>
+
+      <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8">
+          <div className="text-center">
+              <h1 className="text-3xl text-gray-800 font-semibold">
+                  Blog
+              </h1>
+              <p className="mt-3 text-gray-500">
+                  Blogs that are loved by the community. Updated every hour.
+              </p>
+          </div>
+
+          <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+            {news.map((article, index) => 
+            <NewsCard
+              key={index}
+              imageUrl={article.photo_url}
+              date={article.published_datetime_utc}
+              title={article.title}
+              snippet={article.snippet}
+            />)}
+          </div>
+        </section>
       </>
   );
 };
